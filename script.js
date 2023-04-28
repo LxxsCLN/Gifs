3c0f91747b0b2d92642260d9237df84f1a2549a7
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-04-28 11:56:14 -0600

    add search feature

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const img = document.querySelector('img');
+const button = document.querySelector("button")
+const input = document.querySelector("#search")
+const h1 = document.querySelector("h1")
 
 fetch('https://api.giphy.com/v1/gifs/random?api_key=Z0n6ROG5Das3FiyhSXYTwQ2ITEVF1OXX&tag=el chavo del 8', {mode: 'cors'})
     .then(function(response) {
@@ -7,3 +10,26 @@ fetch('https://api.giphy.com/v1/gifs/random?api_key=Z0n6ROG5Das3FiyhSXYTwQ2ITEVF
     .then(function(response) {
         img.src = response.data.images.original.url;
     });
+
+button.addEventListener("click", () => {
+  if (input.value === "") {
+    input.style.color = "white";
+    input.value = h1.innerText;
+  }
+  fetch(`https://api.giphy.com/v1/gifs/random?api_key=Z0n6ROG5Das3FiyhSXYTwQ2ITEVF1OXX&tag=${input.value}`, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+      img.src = response.data.images.original.url;
+  })
+  .then(() => {
+    h1.innerText = input.value.charAt(0).toUpperCase() + input.value.slice(1);
+    input.value = "";
+    input.style.color = "black";
+  })  
+  .catch(function (error){
+    alert("No encontramos ningun gif con ese nombre")
+    console.log(error)
+  }) 
+});
